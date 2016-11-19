a87a015e6cb4d6b72c8be2157e3258e9e474cd19
Marcin Wolicki <marcin.wolicki@gmail.com>
2016-11-20 00:29:34 +0200

    step0 is passing

---
+++ fsharp/Program.fs
@@ -0,0 +1,19 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+
+let rec repl () : unit =
+    printf "user> "
+
+    match Console.ReadLine () with
+    | "#quit" | "#q" -> ()
+    | cmd -> 
+        printfn "%s" cmd
+        repl ()
+
+
+[<EntryPoint>]
+let main argv =
+    repl ()
+    0 // return an integer exit code
